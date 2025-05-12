bde3e66854e08b005682bbc520f7a8f924896660
David <dlozanonav1@educacion.navarra.es>
2025-05-12 14:02:30 +0200

    Fin clase dia 12

---
+++ src/main/java/edu/masanz/dto/MapaDTO.java
@@ -0,0 +1,18 @@
+package edu.masanz.dto;
+
+public class MapaDTO {
+    private long id;
+    private int orden;
+    private String nombre;
+
+    public MapaDTO() {
+        this(0,0,"");
+    }
+
+    public MapaDTO(long id, int orden, String nombre) {
+        this.id = id;
+        this.orden = orden;
+        this.nombre = nombre;
+    }
+}
+
